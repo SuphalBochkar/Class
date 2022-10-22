60a3171712b5c9c492408905651c7b6999a9663f
Finish66 <suphalbochkar@gmail.com>
2022-10-22 23:57:46 +0530

    Teams 22/10 Practise.

---
+++ Python/22pra.py
@@ -0,0 +1,29 @@
+# a = input() #string
+# ex = pow(int(a[0]),int(a[-1]))
+# # print(ex)
+# print((ex),(ex % 2 == 0))
+
+# a = str(input())
+# b = str(input())
+# # c = len(b)
+# print(a[0] == b[-1])
+
+# print("Welcome to fibonacci")
+# n1 = int(input("enter first: "))
+# n2 = int(input("enter second: "))
+# noft = int(input("No of terms u need: "))
+
+# print(n1,n2)
+
+# while(noft-2):
+#     n3 = n1 + n2
+#     n1 = n2
+#     n2 = n3
+#     print(n3)
+#     noft = noft -1
+
+a = str(input())
+b = str(input())
+print(a[-1] in b and a[-2] in b)
+
+
