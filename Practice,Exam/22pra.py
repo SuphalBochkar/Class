38f096029d6c0194eb7677aaf3a5a04c596ffa00
Finish66 <suphalbochkar@gmail.com>
2022-10-27 22:17:07 +0530

    Before-Pratice22

--- Practice,Exam/22pra.py
+++ Practice,Exam/22pra.py
@@ -1,29 +1,56 @@
-# a = input() #string
-# ex = pow(int(a[0]),int(a[-1]))
-# # print(ex)
-# print((ex),(ex % 2 == 0))
+a = input() #string
+ex = pow(int(a[0]),int(a[-1]))
+# print(ex)
+print((ex),(ex % 2 == 0))
 
-# a = str(input())
-# b = str(input())
-# # c = len(b)
-# print(a[0] == b[-1])
+a = str(input())
+b = str(input())
+# c = len(b)
+print(a[0] == b[-1])
 
-# print("Welcome to fibonacci")
-# n1 = int(input("enter first: "))
-# n2 = int(input("enter second: "))
-# noft = int(input("No of terms u need: "))
+print("Welcome to fibonacci")
+n1 = int(input("enter first: "))
+n2 = int(input("enter second: "))
+noft = int(input("No of terms u need: "))
 
-# print(n1,n2)
+print(n1,n2)
 
-# while(noft-2):
-#     n3 = n1 + n2
-#     n1 = n2
-#     n2 = n3
-#     print(n3)
-#     noft = noft -1
+while(noft-2):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3)
+    noft = noft -1
 
 a = str(input())
 b = str(input())
 print(a[-1] in b and a[-2] in b)
+
+list = [10,11,12,13,14]
+length = len(list)
+
+i=0
+while i<length:
+    print(list[i])
+    i  = i+1
+n1=0
+n2=1
+print(n1,n2)
+n3=n1+n2
+print(n3)
+
+n1=n2
+n2=n3
+sum = 0
+i=0
+x = [9,4,3,8,7,2,6,1,5]
+
+length=len(x)
+
+while i<len(x):
+    sum = sum +x[i]
+    i=i+1
+print("the sum of list is ", sum)
+
 
 
