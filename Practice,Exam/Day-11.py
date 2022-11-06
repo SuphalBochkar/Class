5f84e2e4a5f31e5fab166f219f1598f395503507
Finish66 <suphalbochkar@gmail.com>
2022-11-06 22:49:56 +0530

    Printing Even index

--- Practice,Exam/Day-11.py
+++ Practice,Exam/Day-11.py
@@ -1,1 +1,11 @@
 # Even Indexed
+    # s="LANGUAGE"
+    # M-1
+    # print(s[0])
+    # print(s[2])
+    # print(s[4])
+    # print(s[6])
+    # M-2
+    # for i in range (len(s)):
+    #     if i%2 == 0:
+    #         print(s[i])
