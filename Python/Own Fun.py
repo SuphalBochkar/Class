861a6999e4776229be56bd233d98d0be1374902f
Finish66 <suphalbochkar@gmail.com>
2022-11-08 19:17:30 +0530

    Reverse Number

--- Python/Own Fun.py
+++ Python/Own Fun.py
@@ -41,12 +41,11 @@
     #     else:
     #         return (x*(factorial(x-1)))
     # print(factorial(4))
-
 #Reverse A Number
-n=int(input())
-i=0
-while n>0:
-    r=n%10
-    i=(i*10)+r
-    n=n//10
-print(i)
+    # n=int(input())
+    # i=0
+    # while n>0:
+    #     r=n%10
+    #     i=(i*10)+r
+    #     n=n//10
+    # print(i)
